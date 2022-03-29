e2edae6953ed5451ada136c7b0cca4d40f342f9d
Victor Møller Poulsen <victormoeller@gmail.com>
2022-03-29 15:36:33 +0200

    new table

--- extract_data/json_to_csv_one_file.py
+++ extract_data/json_to_csv_one_file.py
@@ -19,7 +19,7 @@ import io
 
 media_list = ["shen_shiwei", "CGTNOfficial", "XHNews", "ChinaDaily", "chenweihua", "CNS1952", "PDChina", "PDChinese", "globaltimesnews", "HuXijin_GT", "XinWen_Ch", "QiushiJournal"]
 
-diplomat_list = ['AmbassadeChine', 'Amb_ChenXu', 'ambcina', 'AmbCuiTiankai', 'AmbLiuXiaoMing','CCGBelfast','ChinaAmbUN','chinacgedi', 'ChinaConsulate', 'ChinaEmbassyUSA','ChinaEmbGermany','ChinaEUMission','ChinaInDenmark','China_Lyon','Chinamission2un','ChinaMissionGva','ChinaMissionVie','chinascio', 'ChineseEmbinUK', 'ChineseEmbinUS', 'ChnMission','CHN_UN_NY', 'consulat_de', 'EUMissionChina','GeneralkonsulDu','MFA_China','SpokespersonCHN', 'SpokespersonHZM','zlj517', 'AmbCina', 'ChinaConSydney', 'ChnEmbassy_jp', 'ChinaEmbOttawa', 'ChinaCGCalgary', 'ChinaCGMTL', 'ChnConsul_osaka', 'ChinainVan']
+diplomat_list = ['AmbassadeChine', 'Amb_ChenXu', 'ambcina', 'AmbCuiTiankai', 'AmbLiuXiaoMing','CCGBelfast','ChinaAmbUN','chinacgedi', 'ChinaConsulate', 'ChinaEmbassyUSA','ChinaEmbGermany','ChinaEUMission','ChinaInDenmark','China_Lyon','Chinamission2un','ChinaMissionGva','ChinaMissionVie','chinascio', 'ChineseEmbinUK', 'ChineseEmbinUS', 'ChnMission','CHN_UN_NY', 'consulat_de', 'EUMissionChina','GeneralkonsulDu','MFA_China','SpokespersonCHN', 'spokespersonHZM', 'SpokespersonHZM','zlj517', 'AmbCina', 'ChinaConSydney', 'ChnEmbassy_jp', 'ChinaEmbOttawa', 'ChinaCGCalgary', 'ChinaCGMTL', 'ChnConsul_osaka', 'ChinainVan']
 
 
 
